4f1c9d17cc464004097d13a4b39a5b8c36ec7143
Mohit Jain <mohitjain9875@gmail.com>
2021-05-24 15:41:19 +0530

    first commit

---
+++ Python Basics/Hello World/app.py
@@ -0,0 +1,10 @@
+# print('hello')
+# print('hi, how are you doing?')
+# print('hello world')
+# name = input('what\'s yor name? ')
+# print(name + ' loves blue color')
+# dob = input('date of birth')
+# dob = int(dob)
+# print(type(dob))
+
+
