2b8ecdea0ec118fb45d7d97ef950ac4d36e261a5
Mohit Jain <mohitjain9875@gmail.com>
2021-06-04 16:29:23 +0530

    Dynamic URL

--- Django Projects/crm1/accounts/views.py
+++ Django Projects/crm1/accounts/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def home(request):
-	return render(request, 'accounts/dashboard.html')
+	orders = Order.objects.all()
+	customers = Customer.objects.all()
+
+	total_customers = customers.count()
+
+	total_orders = orders.count()
+	delivered = orders.filter(status='Delivered').count()
+	pending = orders.filter(status='Pending').count()
+
+
+
+	context = {'orders': orders, 'customers': customers, 'total_orders': total_orders, 'delivered': delivered, 'pending': pending}
+
+	return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
-	return render(request, 'accounts/products.html')
+	products = Product.objects.all()
+	return render(request, 'accounts/products.html', {'products': products})
 
-def customer(request):
-	return render(request, 'accounts/customer.html')
+def customer(request,pk_test):
+	customer = Customer.objects.get(id=pk_test)
+	orders = customer.order_set.all()
+	customer_order = orders.count()
+
+	context = {'customer': customer, 'orders': orders, 'customer_order': customer_order}
+	return render(request, 'accounts/customer.html', context)
